cb5ecc89195e280e188b3a68c220cc8eb6a3b650
Elena <elenaharan@gmail.com>
2024-07-03 06:03:56 +0100

    Finish building reusable table component with render props

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -81,7 +81,11 @@ function Header({ children }) {
     </StyledHeader>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 function Row({ children }) {
   const { columns } = useContext(TableContext);
   return (
