e101e6dc3013df95cbe005aec578f91e61e402e3
Elena <elenaharan@gmail.com>
2024-07-08 07:04:34 +0100

    Add ability to sort cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -7,8 +7,10 @@ import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
-
   const [searchParams] = useSearchParams();
+  if (isLoading) return <Spinner />;
+
+  //1. FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -19,7 +21,14 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
 
-  if (isLoading) return <Spinner />;
+  //2. SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -32,7 +41,8 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => (
             <CabinRow
               cabin={cabin}
