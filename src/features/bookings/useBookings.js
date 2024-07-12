a33fb6f718c39ca9fb4e7ddcb5d352b2ec98c300
Elena <elenaharan@gmail.com>
2024-07-12 07:38:31 +0100

    Add sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -6,19 +6,25 @@ export function useBookings() {
   const [searchParams] = useSearchParams();
   const filterValue = searchParams.get("status");
 
+  //FILTER
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
   // : { field: "totalPrice", value: 5000, method: "gte" };
 
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
