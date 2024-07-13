6744e9c40df3c3bc969350cd538be40cd11491df
Elena <elenaharan@gmail.com>
2024-07-13 09:34:28 +0100

    Finish building reusable pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -34,7 +34,7 @@ function BookingTable() {
           )}
         />
         <Table.Footer>
-          <Pagination count={15} />
+          <Pagination count={5} />
         </Table.Footer>
       </Table>
     </Menus>
