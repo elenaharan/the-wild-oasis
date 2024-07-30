5fef597cffd3968300dcf1f1741c259ad17a6141
Elena <elenaharan@gmail.com>
2024-07-30 08:57:56 +0100

    Add other stats cards for dashboard

--- src/features/dashboard/Stats.jsx
+++ src/features/dashboard/Stats.jsx
@@ -1,5 +1,6 @@
-import { HiOutlineBriefcase } from "react-icons/hi";
+import { HiOutlineBriefcase, HiOutlineChartBar } from "react-icons/hi";
 import Stat from "./Stat";
+import { HiOutlineBanknotes, HiOutlineCalendarDays } from "react-icons/hi2";
 
 function Stats({ bookings, confirmedStays }) {
   const numBookings = bookings.length;
@@ -9,6 +10,24 @@ function Stats({ bookings, confirmedStays }) {
         title="Bookings"
         color="blue"
         icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={numBookings}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={numBookings}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
         value={numBookings}
       />
     </>
