348b7cbb329ad07eea1a5acac8ee21b59bfb3026
Elena <elenaharan@gmail.com>
2024-08-04 09:39:09 +0100

    Add error boundary package to handle react rendering errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ErrorBoundary } from "react-error-boundary";
 import App from "./App.jsx";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
