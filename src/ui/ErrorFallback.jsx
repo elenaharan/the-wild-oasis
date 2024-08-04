9ed1f10f872611e4c95479d66efa2add60ce3d6e
Elena <elenaharan@gmail.com>
2024-08-04 09:50:12 +0100

    Pass call back to error fallback component

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Heading from "./Heading";
 import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -32,13 +33,17 @@ const Box = styled.div`
   }
 `;
 
-function ErrorFallback({ error }) {
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
     <GlobalStyles>
       <StyledErrorFallback>
         <Box>
           <Heading as="h1">Something went wrong 🧐</Heading>
           <p>{error.message}</p>
+          <Button
+            size="large"
+            onClick={resetErrorBoundary}
+          ></Button>
         </Box>
       </StyledErrorFallback>
     </GlobalStyles>
