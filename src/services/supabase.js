3aeeb6e1b77c2401ca57e590bbd4e86d93c7f444
Elena Haran <elenaharan@gmail.com>
2025-03-15 16:10:37 +0000

    update spk

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://bwzhikiaoptxqridhixn.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3emhpa2lhb3B0eHFyaWRoaXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyNTU5NjMsImV4cCI6MjAzMzgzMTk2M30.sysL4haVOnYBJegQHDW29S2bFOMQ9An7GJDD3zZ-XN4";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrb2V3amtyaGl5anNubnVqZHRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNTA1NTMsImV4cCI6MjA1NzYyNjU1M30.XwmG-4HXkIDGlU34Ucr-Bl6hUNu0691HhAFFNL-8aHI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
